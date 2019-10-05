64abee6fe1021b84a04d2e764321e8bed3660d6b
ashishkhuraishy <42002670+ashishkhuraishy@users.noreply.github.com>
2019-10-05 21:20:25 +0530

    added recyclerview & adapter

--- app/src/main/java/com/blogspot/codecampanion/simpletodo/TaskAdapter.java
+++ app/src/main/java/com/blogspot/codecampanion/simpletodo/TaskAdapter.java
@@ -1,6 +1,54 @@
 package com.blogspot.codecampanion.simpletodo;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TaskAdapter extends RecyclerView.ViewHolder {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+    private List<Task> taskList = new ArrayList<>();
+
+    class TaskViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView task;
+
+        TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            task = itemView.findViewById(R.id.textViewTask);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.task_list_activity, parent, false
+        );
+
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        Task currentItem = taskList.get(position);
+
+        holder.task.setText(currentItem.getTask());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
+    }
+
+
 }
